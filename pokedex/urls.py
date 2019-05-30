848d34ca370d96976d9f05b6d3f4f291e8c7e5ce
Xangool <xandreale09@gmail.com>
2019-05-30 14:44:21 -0300

    Listar Criado

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import *
+
+
+app_name = "pokedex"
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('listar',listarPokemons, name = "todos_pokemons")
 ]
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+def listarPokemons (request):
+    pokemon = Pokemon.objects.all()
+
+    contexto = {
+        "todos_pokemons" : pokemon
+    }
+
+    return render(request,'listar.html', contexto)
